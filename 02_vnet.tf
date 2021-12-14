82f6d622f6f7d813db40edea2873bfb89673b151
errormin <smgm030@gmail.com>
2021-12-14 09:42:58 +0900

    step3

---
+++ 02_vnet.tf
@@ -0,0 +1,8 @@
+resource "azurerm_virtual_network" "vmss" {
+  name                = "vmss-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.location
+  resource_group_name = azurerm_resource_group.vmss.name
+  tags                = var.tags # (3)
+}
+#==================================================
