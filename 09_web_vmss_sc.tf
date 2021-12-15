51654be90638deb0a07713ce041647c3ca199e75
errormin <smgm030@gmail.com>
2021-12-15 09:48:49 +0900

    step5testlinuxcode

--- 09_web_vmss_sc.tf
+++ 09_web_vmss_sc.tf
@@ -1,8 +1,8 @@
-resource "azurerm_virtual_machine_extension" "vmss_web_vm_sc" {
+resource "azurerm_virtual_machine_scale_set_extension" "vmss_web_vm_sc" {
   name = "vmss_web_vm_sc"
-  virtual_machine_id = azurerm_virtual_machine.jumpbox.id
-  publisher = "Microsoft.Azure.Extensions"
-  type = "CustomScript"
+  virtual_machine_scale_set_id = azurerm_linux_virtual_machine_scale_set.vmss.id
+  publisher                    = "Microsoft.Azure.Extensions"
+  type                         = "CustomScript"
   type_handler_version = "2.0"
 
   settings = <<SETTINGS
