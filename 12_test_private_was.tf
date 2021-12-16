c7587c9f86f40816e7d724cf47ebc9ec4a549610
errormin <smgm030@gmail.com>
2021-12-16 20:48:37 +0900

    step8

--- 12_test_private_was.tf
+++ 12_test_private_was.tf
@@ -4,9 +4,10 @@ resource "azurerm_lb" "was_vmss" {
   resource_group_name = azurerm_resource_group.vmss.name
 
   frontend_ip_configuration {
-    name                          = "PrivateIPAddress"
+    name                          = "wasPrivateIP"
     subnet_id                     = azurerm_subnet.privatevmss.id
-    private_ip_address_allocation = "Dynamic"
+    private_ip_address            = "10.0.2.20"
+    private_ip_address_allocation = "Static"
   }
 }
 
@@ -19,7 +20,7 @@ resource "azurerm_lb_probe" "wasvmss" { # 로벨 프로브
   resource_group_name = azurerm_resource_group.vmss.name
   loadbalancer_id     = azurerm_lb.was_vmss.id
   name                = "was-running-probe"
-  port                = 8080
+  port                = 8009
 }
 
 resource "azurerm_lb_rule" "waslbnatrule" { # 부하분산 규칙 추가
@@ -27,23 +28,23 @@ resource "azurerm_lb_rule" "waslbnatrule" { # 부하분산 규칙 추가
   loadbalancer_id                = azurerm_lb.was_vmss.id # NAT 규칙을 생성할 LoadBalancer의 ID
   name                           = "was_ilb_rule"
   protocol                       = "Tcp"
-  frontend_port                  = var.application_port # (4) 80번 포트 외부
-  backend_port                   = 8080
+  frontend_port                  = 8009 # (4) 80번 포트 외부
+  backend_port                   = 8009
   backend_address_pool_id        = azurerm_lb_backend_address_pool.wasbpepool.id #백엔드 풀 추가
-  frontend_ip_configuration_name = "PrivateIPAddress"                            # 규칙이 연결된 프런트엔드 IP 구성의 이름
+  frontend_ip_configuration_name = "wasPrivateIP"                                   # 규칙이 연결된 프런트엔드 IP 구성의 이름
   probe_id                       = azurerm_lb_probe.wasvmss.id                   #상태 프로브 추가
 }
 
 resource "azurerm_linux_virtual_machine_scale_set" "wasvmss" {
-  name                            = "privmscaleset"
-  location                        = var.location # (2)
+  name                            = "sm-was-vmss"
+  location                        = azurerm_resource_group.vmss.location # (2)
   resource_group_name             = azurerm_resource_group.vmss.name
   sku                             = "Standard_DS1_v2" # 머신 디스크 크기 선택 및 vmss 개수 지정 
   instances                       = 2                 # vmss 가상머신 개수.
-  admin_username                  = "adminuser"
-  admin_password                  = "#Rlflqhdl21"
+  admin_username                  = "sangmin030"
+  admin_password                  = "!Rlflqhdl21"
   disable_password_authentication = false
-  custom_data    = base64encode("wassh.sh")
+  custom_data                     = base64encode("wassh.sh")
 
   source_image_reference {
     # VM OS 설정및 변경
@@ -51,7 +52,6 @@ resource "azurerm_linux_virtual_machine_scale_set" "wasvmss" {
     offer     = "CentOS"
     sku       = "7.5"
     version   = "latest"
-
   }
 
   os_disk {
@@ -78,7 +78,7 @@ resource "azurerm_linux_virtual_machine_scale_set" "wasvmss" {
       primary                                = true
     }
   }
-  tags = var.vmsstags
+
   extension {
     name                       = "CustomScript"
     publisher                  = "Microsoft.Azure.Extensions"
@@ -88,9 +88,9 @@ resource "azurerm_linux_virtual_machine_scale_set" "wasvmss" {
 
     settings = <<SETTINGS
   {
-      "script": "${filebase64("websh.sh")}"
+      "script": "${filebase64("wassh.sh")}"
   }
   SETTINGS
   }
-  
+
 }
