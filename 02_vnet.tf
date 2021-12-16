9770238247bde02bb5c9db1a8ef5ca61e245ed3d
errormin <smgm030@gmail.com>
2021-12-16 20:47:45 +0900

    step8

--- 02_vnet.tf
+++ 02_vnet.tf
@@ -1,7 +1,7 @@
 resource "azurerm_virtual_network" "vmss" {
   name                = "vmss-vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = var.location
+  location            = azurerm_resource_group.vmss.location
   resource_group_name = azurerm_resource_group.vmss.name
   tags                = var.tags # (3)
 }
